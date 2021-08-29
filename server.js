95b91bfffa963de137cde171e8d73b7567cfdc0c
Eugene Serdyuk <eugene.serdyuk@pm.me>
2021-08-29 21:27:51 +0100

    fix dns lookup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const redis = require('redis').createClient(process.env.REDIS_URL)
 const dns = require('dns')
+const url = require('url')
 const md5 = require('crypto-js/md5')
 const b64 = require('crypto-js/enc-base64url')
 
@@ -18,24 +19,23 @@ function get_key (string) {
 
 // endpoints
 app.post('/api/shorturl', (req, res) => {
-  const url = req.body.url
-  console.log('URL: ' + url)
-  dns.lookup(url, (err, address) => {
-    if (err || !address) {
-      console.log(err)
-      res.json({ error: 'invalid url' })
-    } else {
-      const key = get_key(url)
-      redis.set(key, url, redis.print)
-      res.json({ original_url: url, short_url: key })
-    }
-  })
+  const parsed_url = new url.URL(req.body.url)
+  dns.lookup(parsed_url.protocol ? parsed_url.hostname : parsed_url.toString(),
+    (err, address) => {
+      if (err || !address) {
+        res.json({ error: 'invalid url' })
+      } else {
+        const key = get_key(parsed_url.toString())
+        redis.set(key, parsed_url.toString())
+        res.json({ original_url: parsed_url.toString(), short_url: key })
+      }
+    })
 })
 
 app.get('/api/shorturl/:short_url', (req, res) => {
   redis.get(req.params.short_url, (err, reply) => {
     if (!err && reply) {
-      console.log("REPLY: " + reply)
+      console.log('REPLY: ' + reply)
       res.redirect(reply)
     } else {
       res.json({ error: 'invalid url' })
