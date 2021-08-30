b9d86030bc9d405f30122f98723a75c2a7e49739
Eugene Serdyuk <eugene.serdyuk@pm.me>
2021-08-30 21:28:26 +0100

    add fe

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const uploads = multer({dest: 'uploads/'})
 const PORT = process.env.PORT || 3000
 
 express().
+  use('/public', express.static(process.cwd() + '/public')).
   use(cors({ optionsSuccessStatus: 200 })).
   use(express.urlencoded({ extended: true })).
+  get('/', (req, res) => {res.sendFile(process.cwd() + '/views/index.html')}).
   post('/api/fileanalyse', uploads.single('upfile'), (req, res) => {
     return res.json({
       name: req.file.originalname,
